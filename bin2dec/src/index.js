45b92449bee4716154e6c9403671ebc7fe04a32d
Sanskar Sharma <sanskarsharma@Sanskars-MacBook-Air.local>
2022-12-26 12:54:10 +0530

    adding src folder with index.js(basic structure) and styles.js (for css)

---
+++ bin2dec/src/index.js
@@ -0,0 +1,32 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom';
+
+import {
+    StyledForm,
+    BinaryTextInput,
+    Label,
+    Button,
+    DecimalTextInput,
+    Field
+} from './styles'
+
+function App(){
+    const [binaryText, setBinaryText] = useState('');
+    const [decimalText, setDecimalText] = useState('');
+    const [errorMessage, setErrorMessage] = useState('');
+
+    // perform the conversion on form submit
+    const onFormSubmit = e => {
+
+    }
+    
+    const reverseBinaryText;
+    
+    const result;
+
+    return (
+        <>
+            <h1>Binary to Decimal Converter</h1>
+        </>
+    )
+}
