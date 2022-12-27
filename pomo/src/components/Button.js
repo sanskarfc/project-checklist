5734b26f9e63333964c56bd6d47acca606b62db5
Sanskar Sharma <sanskarsharma@Sanskars-MacBook-Air.local>
2022-12-27 23:04:29 +0530

    completed the button component

---
+++ pomo/src/components/Button.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Button ({ id, onClick, children, icon }){
+    return (
+        <button id={id} onClick={onClick}>
+            {icon} {children}
+        </button>
+    )
+}
+
+export default Button;
