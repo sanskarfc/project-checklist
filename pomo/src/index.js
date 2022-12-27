efe15d221e7312798911c4724f1dd68a82e5035c
Sanskar Sharma <sanskarsharma@Sanskars-MacBook-Air.local>
2022-12-27 22:41:28 +0530

    updated index.js code

--- pomo/src/index.js
+++ pomo/src/index.js
@@ -16,24 +16,72 @@ class App extends React.Component {
         timerRunning: false
     };
 
-    increment = mode => {
-        
+    increment = mode => { 
+        if(this.state.timerRunning){
+            return;
+        }
+
+        if(mode === "session" && this.state.session_length <= 59){
+            this.setState({
+                session_length: this.state.session_length + 1,
+                timer: this.state.timer + 60
+            });
+        } else if(mode === "break" && this.state.break_length <= 59){
+            this.setState({
+                break_length: this.state.break_length + 1
+            })
+        }
     };
 
     decrement = mode => {
-
+        if(this.state.timerRunning){
+            return;
+        }
+        
+        if(mode === "session" && this.state.session_length >= 2){
+            this.setState({
+                session_length: this.state.session_length - 1,
+                timer: this.state.timer - 60
+            });
+        } else if(mode === "break" && this.state.break_length >= 2) {
+            this.setState({
+                break_length: this.state.break_length - 1
+            });
+        }
     };
 
     reset = () => {
+        this.beep.pause(); // pause the timer 
+        this.beep.currentTime = 0; // change the timer to zero
 
+        this.setState({
+            break_length: 5,
+            session_length: 25,
+            timerRunning: false,
+            timer: 1500,
+            mode: "Session"
+        });
+
+        clearInterval(this.countdown);
     };
 
     pauseTimer = () => {
-
+        clearInterval(this.countdown); // what is this ??? 
+        this.setState({timerRunning: false}); // timerRunning --> false because we are pausing 
     };
 
     countDownTimer = seconds => {
-        
+        clearInterval(this.countdown);
+
+        this.countdown = setInterval(() => {
+            if(this.state.timer === 0 && this.state.mode === "Session") {
+                this.beep.play();
+                this.setState({
+                    mode: "Break",
+                    timer: this.state.break_length * 60 + 1
+                });
+            }
+        })
     };
 
     displayTimeLeft = seconds => {
